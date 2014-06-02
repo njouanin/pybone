ccf030155e5e48e3e2d7a94a1ada5edea560bad4
Nico <nicolas.jouanin@gmail.com>
2014-06-02 21:14:48 +0200

    Rename and refactor

--- pybone/__init__.py
+++ pybone/__init__.py
@@ -1,18 +1,17 @@
-import asyncio
 from pybone.board import Board
 
 VERSION = (0, 0, 1, 'alpha', 0)
 
 
-def _checkRequirements():
+def _check_requirements():
     #Check python >= 3.4
     import sys
     if sys.version_info < (3, 4):
         raise ImportError("Python 3.4 or more is required")
 
-_checkRequirements()
+_check_requirements()
 
 
 #setup version
-from pybone._version import get_pretty_version
+from ._version import get_pretty_version
 __version__ = get_pretty_version(VERSION)
