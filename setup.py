ee42a31f8fb8ae64848141a366c75d4809f01867
Nico <nicolas.jouanin@gmail.com>
2014-08-17 18:16:28 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -9,5 +9,14 @@ setup(
   description="Python library for managing beagleboard hardware I/O",
   url="https://github.com/njouanin/pybone",
   license='GPLv3',
-  packages=find_packages(exclude=['tests'])
+  packages=find_packages(exclude=['tests']),
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: System :: Hardware :: Hardware Drivers',
+    'Topic :: System :: Operating System Kernels :: Linux'
+  ]
 )
