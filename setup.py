d0c45b09970dcbf2d66e79fd7d2636da9d445c46
Nico <nicolas.jouanin@gmail.com>
2014-08-17 20:56:17 +0200

    Add licensing informations

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
-from setuptools import setup, find_packages # Always prefer setuptools over distutils
-from codecs import open # To use a consistent encoding
-from os import path
+# Copyright (C) 2014  Nicolas Jouanin
+#
+# This file is part of pybone.
+#
+# Pybone is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pybone is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from setuptools import setup, find_packages
 from pybone import version_info
 
 setup(
