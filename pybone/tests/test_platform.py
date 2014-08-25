c9fe1c5028bc9e09030ef7b67e00f47985d7d610
Nico <nicolas.jouanin@gmail.com>
2014-08-25 22:50:13 +0200

    Update tests

--- pybone/tests/test_platform.py
+++ pybone/tests/test_platform.py
@@ -4,14 +4,17 @@ import unittest
 from unittest.mock import patch
 from pybone.bone import Platform
 
+
 class PlatformTest(unittest.TestCase):
 
     @patch('pybone.bone.platform')
-    def test_init_platform(self, mock_platform):
-        p=Platform()
+    @patch('pybone.bone.multiprocessing')
+    def test_init_platform(self, mock_multiprocessing, mock_platform):
+        Platform()
         mock_platform.system.assert_called_once_with()
         mock_platform.release.assert_called_once_with()
         mock_platform.processor.assert_called_once_with()
+        mock_multiprocessing.cpu_count.assert_called_once_with()
 
 if __name__ == '__main__':
     unittest.main()
